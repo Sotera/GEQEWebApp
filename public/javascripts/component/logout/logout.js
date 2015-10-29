9e44b314db13e6561b3d25e6b8b1d877e41a9fb6
Justin <justin.lueders@gmail.com>
2015-10-29 10:43:04 -0500

    initial commit

---
+++ public/javascripts/component/logout/logout.js
@@ -0,0 +1,17 @@
+angular.module('logout', ['ngCookies'])
+	.factory('logout', function () {
+		return {
+
+		};
+	})
+	.directive('logout', ['$rootScope', 'setFullNameMsg' ,function ($rootScope,setFullNameMsg) {
+		return{
+			restrict: 'AE',
+			templateUrl: '/views/partials/logout',
+			controller: ['$scope',function($scope){
+				setFullNameMsg.listen( function(){
+						$scope.fullname = $rootScope.fullname;
+				});
+			}]
+		};
+	}]);
